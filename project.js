415b021d96d4ebb40cb5be055aff992136ef5110
mohammadahsanghazanfar <148885644+mohammadahsanghazanfar@users.noreply.github.com>
2023-10-24 08:33:45 -0700

    Add files via upload

---
+++ project.js
@@ -0,0 +1,7 @@
+const list=document.querySelector('#li5')
+const inp=document.querySelector('#inp')
+const forms=document.querySelector('#frm')
+
+list.addEventListener('click',function(){
+       list.innerText="Ahsan"
+})
